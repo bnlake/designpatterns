a2102b640a459026074cec3d4d6551b4e47796a2
Brian Lake <blake@krames.com>
2022-12-19 21:16:53 -0600

    create addtocartcommand from template

---
+++ command/Commands/AddToCartCommand.cs
@@ -0,0 +1,22 @@
+﻿using command.Interfaces;
+
+namespace command.Commands
+{
+    internal class AddToCartCommand : ICommand
+    {
+        public bool CanExecute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Undo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
