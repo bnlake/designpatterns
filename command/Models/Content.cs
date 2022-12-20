bc43502f499dcf61188758ab3be26c11a93f5d39
Brian Lake <blake@krames.com>
2022-12-19 21:57:28 -0600

    implement content class and contentrepo

--- command/Models/Content.cs
+++ command/Models/Content.cs
@@ -2,7 +2,7 @@
 {
     internal class Content
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string? Name { get; set; }
         public string? Body { get; set; }
     }
---
+++ command/Persistance/ContentRepository.cs
@@ -0,0 +1,37 @@
+﻿using command.Interfaces;
+using command.Models;
+
+namespace command.Persistance
+{
+    /// <summary>
+    /// This class simulates an in-memory database.
+    /// This lesson is not about the repository pattern, therefore
+    /// we are implementing a very rudimentary repository
+    /// </summary>
+    internal class ContentRepository : IContentRepository
+    {
+        private readonly IList<Content> contents = new List<Content>();
+
+        public ContentRepository()
+        {
+            contents.Add(new Content { Id = 1, Name = "Sore throat treatment", Body = "Here's a piece of content to help with sore throat" });
+            contents.Add(new Content { Id = 2, Name = "Migraine treatment", Body = "Content that teaches you how to treat migraines" });
+            contents.Add(new Content { Id = 3, Name = "Treating sleep apnea", Body = "Can't sleep? Try reading a book. That usually knocks me out" });
+        }
+        public void Create(Content content)
+        {
+            if (content is null) return;
+            contents.Add(content);
+        }
+
+        public Content? GetContent(int id)
+        {
+            return contents.FirstOrDefault(c => c.Id.Equals(id));
+        }
+
+        public void Delete(Content content)
+        {
+            contents.Remove(content);
+        }
+    }
+}
