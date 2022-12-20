a23fec538ab01cf31fd9a59d019e53bb20242e48
Brian Lake <blake@krames.com>
2022-12-19 21:23:49 -0600

    implement constructor for addtocart command

--- command/Commands/AddToCartCommand.cs
+++ command/Commands/AddToCartCommand.cs
@@ -4,6 +4,22 @@ namespace command.Commands
 {
     internal class AddToCartCommand : ICommand
     {
+        /**
+         * Commands should be aware of any data it requires in order
+         * to execute, verify, or undo its capabilities at any point in time.
+         * Using dependency injection via constructor ensures the command
+         * has what it needs without dependencies at runtime
+         */
+        private readonly Content Content;
+        private readonly IShoppingCartRepository ShoppingCartRepository;
+        private readonly IContentRepository ContentRepository;
+
+        public AddToCartCommand(IShoppingCartRepository shoppingCartRepository, IContentRepository contentRepository, Content content)
+        {
+            Content = content;
+            ShoppingCartRepository = shoppingCartRepository;
+            ContentRepository = contentRepository;
+        }
         public bool CanExecute()
         {
             throw new NotImplementedException();
