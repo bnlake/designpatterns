03b68b63365bf2e18e53747f01939963513b3a52
Brian Lake <blake@krames.com>
2022-12-19 21:13:44 -0600

    create commandmanager class

---
+++ command/CommandManager.cs
@@ -0,0 +1,27 @@
+﻿using command.Interfaces;
+
+namespace command
+{
+    internal class CommandManager
+    {
+        private Stack<ICommand> commands = new Stack<ICommand>();
+
+        public void Invoke(ICommand command)
+        {
+            if (command.CanExecute())
+            {
+                command.Execute();
+                commands.Push(command);
+            }
+        }
+
+        public void Undo()
+        {
+            while (commands.Count > 0)
+            {
+                var command = commands.Pop();
+                command.Undo();
+            }
+        }
+    }
+}
