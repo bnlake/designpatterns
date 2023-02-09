0a5e4e4ef9bbfa6f638850d37f6cfc9abfed5845
Brian Lake <blake@krames.com>
2023-02-08 21:02:31 -0600

    created a base handler class

---
+++ chainofresponsibility/Handlers/BaseHandler.cs
@@ -0,0 +1,25 @@
+﻿using chainofresponsibility.Models;
+
+namespace chainofresponsibility.Handlers
+{
+    internal class BaseHandler : IHandler
+    {
+        protected IHandler? next;
+
+        public BaseHandler(IHandler? next)
+        {
+            this.next = next;
+        }
+
+        public IEnumerable<Content> Handle(int identifier)
+        {
+            // Imitate the Null pattern as our Base Class
+            return new List<Content>();
+        }
+
+        public void SetNext(IHandler h)
+        {
+            this.next = h;
+        }
+    }
+}
