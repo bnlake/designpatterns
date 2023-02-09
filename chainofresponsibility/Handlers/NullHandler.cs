29f687ea854d93bb5c59fbb12300961311901736
Brian Lake <blake@krames.com>
2023-02-08 21:33:20 -0600

    implement a null handler

---
+++ chainofresponsibility/Handlers/NullHandler.cs
@@ -0,0 +1,14 @@
+﻿using chainofresponsibility.Models;
+
+namespace chainofresponsibility.Handlers
+{
+    internal class NullHandler : BaseHandler
+    {
+        public NullHandler(IHandler? next) : base(next) { }
+
+        public override IEnumerable<Content> Handle(int identifier)
+        {
+            return new List<Content>();
+        }
+    }
+}
